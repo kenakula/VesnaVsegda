0a4e0473bca7849ed186f89dcb48cebc7b256ef6
Kenan Agaev <kenkosh@yandex.ru>
2020-04-11 13:00:08 +0300

    Оптимизированы изображения

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('images', function() {
   return gulp.src('source/img/**/*.{png,jpg,svg}')
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
+      imagemin.mozjpeg({progressive: true}),
       imagemin.svgo()
     ]))
     .pipe(gulp.dest('source/img'));
